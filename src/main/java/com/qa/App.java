77f6bf272265a9d0fd0e798835fe6a5d019a70ec
elevatemeaabarna <elevatemeaabarna@qa.com>
2025-09-17 10:37:07 +0100

    changes1

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(sayHelloToSomeone("Sriabarna"));
+        System.out.println(sayHelloToSomeone("Shafina! How are you?"));
         System.out.println(sayGoodbye());
     }
 
